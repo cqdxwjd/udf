419c94dd5b12e705ea841fef16fd799856f6dde9
cqdxwjd <cqdxwjd@163.com>
2020-05-29 09:41:24 +0800

    first commit

---
+++ src/main/java/StdCoordinateY.java
@@ -0,0 +1,13 @@
+import com.yunlizhihui.platform.gis.sdk.utils.TransformUtil;
+import com.yunlizhihui.platform.gis.sdk.utils.YunliCoordinate;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class StdCoordinateY extends UDF {
+    private static TransformUtil transformUtil = new TransformUtil();
+    private static YunliCoordinate yunliCoordinate = null;
+
+    public Double evaluate(Double x, Double y) {
+        yunliCoordinate = transformUtil.convertCoordinate(x, y, "EPSG:2432", "EPSG:4326");
+        return yunliCoordinate.getY();
+    }
+}
